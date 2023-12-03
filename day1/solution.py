84e8b6a33e2e5871f2a433af56eb00a4ac6a2c9b
Hunter19823 <hspragg@asu.edu>
2023-12-03 14:20:30 -0700

    Fixed solution.py

--- day1/solution.py
+++ day1/solution.py
@@ -5,13 +5,20 @@ with open('input.txt', 'r') as f:
         # Sum all the numbers
         sum(
             [
-                # Parse the number made from the first and last character
-                int(f'{numeric[0]}{numeric[-1]}')
-                # Loop through every line in the file
-                for line in f.readlines()
-                # Loop through every character in the line,
-                # selecting only numeric characters
-                for numeric in line if numeric.isdigit()
+                # Take the first and last numeric characters, put them together, then
+                # parse them as an integer
+                int(f"{numbers[0]}{numbers[-1]}")
+                # Map all the lines to only numeric sequences.
+                for numbers in map(
+                    # Use the following function to map a line to a numeric sequence
+                    lambda line: ''.join([
+                        # Only keep the numeric characters from the
+                        # current
+                        character
+                        for character in line if character.isdigit()
+                    ]),
+                    f.readlines()
+                )
             ]
         )
     )
